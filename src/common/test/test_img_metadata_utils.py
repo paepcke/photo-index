8961f0c50facb6487b4600d4e65230628d90b31c
Andreas Paepcke <paepcke@cs.stanford.edu>
2025-12-03 16:59:43 -0800

    Lots more metadata writing corrections

--- src/common/test/test_img_metadata_utils.py
+++ src/common/test/test_img_metadata_utils.py
@@ -2,7 +2,7 @@
 # @Author: Andreas Paepcke
 # @Date:   2025-12-02 14:48:37
 # @Last Modified by:   Andreas Paepcke
-# @Last Modified time: 2025-12-03 14:44:00
+# @Last Modified time: 2025-12-03 16:59:09
 
 import os
 from pathlib import Path
@@ -10,6 +10,8 @@ import shutil
 import unittest
 from PIL import Image
 from tempfile import TemporaryDirectory
+
+import ffmpeg
 
 from common.img_metadata_utils import ImgMDExplorer, FieldType, ExifToolWriteError
 class MDUtilsTester(unittest.TestCase):
@@ -20,6 +22,9 @@ class MDUtilsTester(unittest.TestCase):
         # Create self.jpg_filename and self.png_filename
         # containing test images:
         cls.create_tst_img()
+
+        # Create .mp4 and .mov test movies
+        cls.create_tst_mov()
         cls.tmp_dir = TemporaryDirectory(prefix='metadata_tsts', dir='/tmp')
 
     @classmethod
@@ -31,9 +36,14 @@ class MDUtilsTester(unittest.TestCase):
         for file_path in Path(self.tmp_dir.name).iterdir():
             if file_path.is_file():  # Check if it's a file
                 file_path.unlink()  # Delete the file
+
         # Copy fresh .jpg and .png files to the temp dir:
         shutil.copy(self.jpg_filename, self.tmp_dir.name)
         shutil.copy(self.png_filename, self.tmp_dir.name)
+
+        # Same for .mp4 and .mov movies:
+        shutil.copy(self.mp4_filename, self.tmp_dir.name)
+        shutil.copy(self.qt_filename, self.tmp_dir.name)
 
         # Get a metadata accessor:
         self.img_explorer = ImgMDExplorer()
@@ -44,37 +54,29 @@ class MDUtilsTester(unittest.TestCase):
         md = self.img_explorer.read_fields(self.jpg_filename)
         # We should have an array of one dict,
         # i.e. the fieds of self.jpg_filename:
-        self.assertEqual(type(md), list)
-        self.assertEqual(len(md), 1)
-        fld_dict = md[0]
         # Some of the expected fields in the first
         # (and only) dict of the md array:
         #
         #    'File:FileType': 'JPEG', 
         #    'File:FileTypeExtension': 'JPG', 
         #    'File:MIMEType': 'image/jpeg',         
-        self.assertEqual(type(fld_dict), dict)
-        self.assertEqual(fld_dict['FileType'], 'JPEG')
-        self.assertEqual(fld_dict['FileTypeExtension'], 'jpg')
-        self.assertEqual(fld_dict['MIMEType'], 'image/jpeg')
+        self.assertEqual(type(md), dict)
+        self.assertEqual(md['File:FileType'], 'JPEG')
+        self.assertEqual(md['File:FileTypeExtension'].lower(), 'jpg')
+        self.assertEqual(md['File:MIMEType'], 'image/jpeg')
 
     def test_empty_png_md(self):
         md = self.img_explorer.read_fields(self.png_filename)
-        # We should have an array of one dict,
-        # i.e. the fieds of self.jpg_filename:
-        self.assertEqual(type(md), list)
-        self.assertEqual(len(md), 1)
-        fld_dict = md[0]
         # Some of the expected fields in the first
         # (and only) dict of the md array:
         #
         #    'File:FileType': 'PNG', 
         #    'File:FileTypeExtension': 'PNG', 
         #    'File:MIMEType': 'image/png',         
-        self.assertEqual(type(fld_dict), dict)
-        self.assertEqual(fld_dict['FileType'], 'PNG')
-        self.assertEqual(fld_dict['FileTypeExtension'], 'png')
-        self.assertEqual(fld_dict['MIMEType'], 'image/png')
+        self.assertEqual(type(md), dict)
+        self.assertEqual(md['File:FileType'], 'PNG')
+        self.assertEqual(md['File:FileTypeExtension'].lower(), 'png')
+        self.assertEqual(md['File:MIMEType'], 'image/png')
 
     def test_one_fld_one_file(self):
         md = self.img_explorer.read_fields(
@@ -82,7 +84,7 @@ class MDUtilsTester(unittest.TestCase):
             fld_nms='FileType',
             fld_type=FieldType.File
             )
-        self.assertEqual(md['FileType'], 'JPEG')
+        self.assertEqual(md['File:FileType'], 'JPEG')
 
     def test_two_flds_two_files(self):
         md = self.img_explorer.read_fields(
@@ -92,8 +94,8 @@ class MDUtilsTester(unittest.TestCase):
             )
         jpg_md = md[0]
         png_md = md[1]
-        self.assertEqual(jpg_md['FileType'], 'JPEG')
-        self.assertEqual(png_md['FileType'], 'PNG')
+        self.assertEqual(jpg_md['File:FileType'], 'JPEG')
+        self.assertEqual(png_md['File:FileType'].lower(), 'png')
 
     def test_write_one_exif_fld(self):
     
@@ -139,6 +141,8 @@ class MDUtilsTester(unittest.TestCase):
                 fld_type=FieldType.EXIF
             )
 
+
+
     # ------------ Utilities --------------
     @classmethod
     def create_tst_img(cls):
@@ -168,6 +172,35 @@ class MDUtilsTester(unittest.TestCase):
         except Exception as e:
             print(f"An error occurred: {e}")
         return (cls.png_filename, cls.jpg_filename)
+
+    @classmethod
+    def create_tst_mov(cls):
+
+        # Create an mp4 est clip:
+        
+        cls.mp4_filename = os.path.join(cls.cur_dir,"mp4_movie.mp4")
+
+        # Video input
+        video = ffmpeg.input('testsrc=duration=2:size=1280x720:rate=30', f='lavfi')
+        # Audio input
+        audio = ffmpeg.input('sine=frequency=1000:duration=2', f='lavfi')        
+
+        (ffmpeg
+        .output(video, audio, cls.mp4_filename, pix_fmt='yuv420p')
+        .overwrite_output()
+        .run()
+        )
+
+        # And a Quicktime movie:
+        cls.qt_filename = os.path.join(cls.cur_dir,"qt_movie.mov")
+
+        (ffmpeg
+            .input('testsrc=duration=2:size=1280x720:rate=30', f='lavfi')
+            .output(cls.qt_filename, pix_fmt='yuv420p')
+            .overwrite_output()
+            .run()
+        )        
+
 
 # ------------------------ Main ------------
 if __name__ == '__main__':
